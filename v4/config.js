101a93282638f2a7168d1cfe63270c08daf6a7e0
robsartini <robsartini@gmail.com>
2026-03-29 18:31:23 +0200

    Update config.js

--- v4/config.js
+++ v4/config.js
@@ -1,5 +1,5 @@
 // === CONFIGURAZIONE PRIVATA SHELLY ===
 // https://shelly-85-eu.shelly.cloud/v1/device/stats/?id=bcff4dfd05f7&auth_key=MWVmNTNmdWlk1D2ED53C10E5B83DEBC778C9166C665959FCD9011942DCE39E3D0E751BC18B9D11E61E7A595BE697&channel=0&date_range=today
 const SHELLY_CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxQwn39BH2yDaFxbZAawWDWQHQA3vohTuoqtcHipQrY6cp5ZMuht75C3pibJSM5QLVaOw/exec"
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzud8YEW4f6hPOXMaNA2owkL9k2xe5gIddHmWT0skVAqsGJhYy_VAk__KX0yCUuKf6suQ/exec"
 };
