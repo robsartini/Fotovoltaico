c131b9d96ac33a8c0cb43a6165c40b26716990a1
robsartini <robsartini@gmail.com>
2026-03-29 19:38:50 +0200

    Delete v4 directory

--- v4/config.js
+++
@@ -1,5 +0,0 @@
-// === CONFIGURAZIONE PRIVATA SHELLY ===
-// https://shelly-85-eu.shelly.cloud/v1/device/stats/?id=bcff4dfd05f7&auth_key=MWVmNTNmdWlk1D2ED53C10E5B83DEBC778C9166C665959FCD9011942DCE39E3D0E751BC18B9D11E61E7A595BE697&channel=0&date_range=today
-const SHELLY_CONFIG = {
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxhukPh9RPxSt6a-PQExgtTcs4l9dGemqkcPDz5gjqNZ5FTJstscH4x3gGIBcO9uQtUGw/exec"
-};
